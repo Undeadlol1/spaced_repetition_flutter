e0da98a2843ec50b44bdd2c3fb6526967ea1b57c
Mihail Paley <undeadlol1@mail.ru>
2020-01-05 18:23:34 +0500

    add CardCreateForm

--- lib/screens/create_card_screeen.dart
+++ lib/screens/create_card_screeen.dart
@@ -12,16 +12,72 @@ class CreateCardScreen extends StatelessWidget {
           ),
           backgroundColor: Colors.black,
         ),
-        body: Container(
+        body: CardCreateForm());
+  }
+}
+
+class CardCreateForm extends StatefulWidget {
+  @override
+  CardCreateFormState createState() {
+    return CardCreateFormState();
+  }
+}
+
+class CardCreateFormState extends State<CardCreateForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _frontTextController = TextEditingController();
+  final _backTextController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
             child: TextFormField(
-          autovalidate: true,
-          validator: (text) {
-            if (text.length > 100) {
-              return 'Text is too long';
-            }
-            return null;
-          },
-          onEditingComplete: () => Navigator.pushNamed(context, '/'),
-        )));
+              decoration: InputDecoration(labelText: 'Front'),
+              controller: _frontTextController,
+              validator: (value) {
+                final emptyError = 'Please enter some text';
+                if (value.trim().isEmpty) return emptyError;
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: TextFormField(
+              controller: _backTextController,
+              decoration: InputDecoration(labelText: 'Back'),
+              validator: (value) {
+                final emptyError = 'Please enter some text';
+                if (value.trim().isEmpty) return emptyError;
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Center(
+              child: RaisedButton(
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    Scaffold.of(context).showSnackBar(
+                        SnackBar(content: Text('Processing Data')));
+                    print('front ${_frontTextController.text}');
+                    print('back ${_backTextController.text}');
+                  }
+                },
+                child: Text('Submit'),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
